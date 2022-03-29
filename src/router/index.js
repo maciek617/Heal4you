61a922a2b4cea3f587d1acbb9682f65a124d4915
Maciej Gosztyła <r3cenzialny@gmail.com>
2022-03-29 16:50:26 +0200

    Add route guards to main app page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import HomeView from '../views/HomeView.vue';
 
 const routes = [
@@ -17,11 +18,45 @@ const routes = [
     name: 'signup',
     component: () => import('../views/auth/SignupView.vue'),
   },
+  {
+    path: '/heal4you',
+    name: 'heal4you',
+    component: () => import('../views/AppView.vue'),
+    meta: {
+      requiresAuth: true,
+    },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next();
+    } else {
+      alert('You have to be logged in to see te content.');
+      next('/login');
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
